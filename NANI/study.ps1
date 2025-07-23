1d6bfce15898292ba37f4f12871185b137dc8e89
onslaught2342 <onslaught2342@outlook.com>
2025-07-23 21:18:25 +0500

    fix redownload bug

--- NANI/study.ps1
+++ NANI/study.ps1
@@ -1,5 +1,12 @@
-
-Install-Module BurntToast -Scope CurrentUser -Force
-$imagePath = "$env:APPDATA\nani.jpg"
-Invoke-WebRequest "https://assets.st-note.com/img/1726514624-gBoKDycXILUmFa9zTlPh1Cf8.jpg" -OutFile $imagePath
+$imagePath = "$env:APPDATA\Microsoft\study\images\nani.jpg"
+if (!(Test-Path (Split-Path $imagePath))) {
+    New-Item -Path (Split-Path $imagePath) -ItemType Directory -Force | Out-Null
+}
+if (!(Test-Path $imagePath)) {
+    Invoke-WebRequest "https://assets.st-note.com/img/1726514624-gBoKDycXILUmFa9zTlPh1Cf8.jpg" -OutFile $imagePath
+}
+if (-not (Get-Module -ListAvailable -Name BurntToast)) {
+    Install-Module BurntToast -Scope CurrentUser -Force -AllowClobber
+}
+Import-Module BurntToast
 New-BurntToastNotification -Text "OMAEE WA MOU SHINDEIRU", "N A N I ?! 🔥💀" -AppLogo $imagePath
